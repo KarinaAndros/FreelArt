969fcd4f7fc70aadc13c135a3793aa14dd58c161
KarinaAndros <84373468+KarinaAndros@users.noreply.github.com>
2022-04-01 17:05:39 +0300

    'изменено'

--- database/migrations/2022_03_22_183219_create_subscriptions_table.php
+++ database/migrations/2022_03_22_183219_create_subscriptions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -15,8 +16,10 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('user_email');
             $table->string('status')->default('действительна');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
